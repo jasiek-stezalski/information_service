3bd34b8841828d48ab1866c17111d5da26c93a17
jasiek-stezalski <janstezalski@gmail.com>
2018-04-05 10:50:36 +0200

    Remove postgresql dependency, add ArticleService

---
+++ src/main/java/com/pack/information_service/service/ArticleService.java
@@ -0,0 +1,4 @@
+package com.pack.information_service.service;
+
+public interface ArticleService {
+}
---
+++ src/main/java/com/pack/information_service/service/ArticleServiceImpl.java
@@ -0,0 +1,7 @@
+package com.pack.information_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleServiceImpl implements ArticleService{
+}
